99175e37b3cb3ad58061452bbc853f0ecf1f2c4a
marcobonici <bonici.marco@gmail.com>
2022-09-04 00:59:56 +0200

    First working version of Capse..jl

---
+++ src/Capse.jl
@@ -0,0 +1,49 @@
+module Capse
+
+using Base: @kwdef
+using SimpleChains
+
+function maximin_input!(x, in_MinMax)
+    for i in 1:6
+        x[i] -= in_MinMax[i,1]
+        x[i] /= (in_MinMax[i,2]-in_MinMax[i,1])
+    end
+end
+
+function inv_maximin_output!(x, out_MinMax)
+    for i in 1:2499
+        x[i] *= (out_MinMax[i,2]-out_MinMax[i,1])
+        x[i] += out_MinMax[i,1]
+    end
+end
+
+abstract type AbstractTrainedEmulators end
+
+@kwdef mutable struct SimpleChainsEmulator <: AbstractTrainedEmulators
+    Architecture
+    Weights
+end
+
+abstract type AbstractCℓEmulators end
+
+@kwdef mutable struct CℓTTEmulators <: AbstractCℓEmulators
+    TrainedEmulator::AbstractTrainedEmulators
+    InMinMax::Matrix{Float64} = zeros(6,2)
+    OutMinMax::Array{Float64} = zeros(2499,2)
+end
+
+function compute_Cℓ(input_params, CℓEmulator::AbstractCℓEmulators)
+    input = deepcopy(input_params[1:6])
+    maximin_input!(input, CℓEmulator.InMinMax)
+    output = Array(run_emulator(input, CℓEmulator.TrainedEmulator))
+    inv_maximin_output!(output, CℓEmulator.OutMinMax)
+    return output .* exp(input_params[7]-1.)
+    #TODO: check Aₛ, maybe not so linear as you expected due to lensing!
+end
+
+function run_emulator(input, trained_emulator::SimpleChainsEmulator)
+    return trained_emulator.Architecture(input, trained_emulator.Weights)
+end
+
+
+end # module

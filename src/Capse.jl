197f72ce6eb8aabb502abee4d5952015ee33861d
marcobonici <bonici.marco@gmail.com>
2022-09-07 10:37:59 +0200

    Improved and generalized a few hardcoded settings

--- src/Capse.jl
+++ src/Capse.jl
@@ -4,14 +4,14 @@ using Base: @kwdef
 using SimpleChains
 
 function maximin_input!(x, in_MinMax)
-    for i in 1:8
+    for i in 1:length(x)
         x[i] -= in_MinMax[i,1]
         x[i] /= (in_MinMax[i,2]-in_MinMax[i,1])
     end
 end
 
 function inv_maximin_output!(x, out_MinMax)
-    for i in 1:2499
+    for i in 1:length(x)
         x[i] *= (out_MinMax[i,2]-out_MinMax[i,1])
         x[i] += out_MinMax[i,1]
     end
@@ -26,7 +26,7 @@ end
 
 abstract type AbstractCℓEmulators end
 
-@kwdef mutable struct CℓTTEmulators <: AbstractCℓEmulators
+@kwdef mutable struct CℓEmulators <: AbstractCℓEmulators
     TrainedEmulator::AbstractTrainedEmulators
     InMinMax::Matrix{Float64} = zeros(8,2)
     OutMinMax::Array{Float64} = zeros(2499,2)
@@ -38,7 +38,6 @@ function compute_Cℓ(input_params, CℓEmulator::AbstractCℓEmulators)
     output = Array(run_emulator(input, CℓEmulator.TrainedEmulator))
     inv_maximin_output!(output, CℓEmulator.OutMinMax)
     return output .* exp(input_params[1]-3.)
-    #TODO: check Aₛ, maybe not so linear as you expected due to lensing!
 end
 
 function run_emulator(input, trained_emulator::SimpleChainsEmulator)

c3d9f02fc143c11c4dc0f5bc134ca4c64c9907f9
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 21:00:16 +0000

    build based on 7b475b2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Capse.jl","page":"Home","title":"Capse.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Capse.jl","category":"page"}]
+}

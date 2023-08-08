851ec1ea06c1e287eedb3e0179321fdcc9c51a79
Marco Bonici <bonici.marco@gmail.com>
2023-08-08 23:43:17 +0200

    Adding docs

---
+++ docs/src/api.md
@@ -0,0 +1,11 @@
+# API reference
+
+## `Capse`
+
+The most commonly used function from the QuadGK package is the `quadgk` function, used to compute numerical integrals (by h-adaptive Gauss–Kronrod quadrature):
+
+```@docs
+Capse.CℓEmulator
+Capse.get_Cℓ
+Capse.get_ℓgrid
+```
--- src/Capse.jl
+++ src/Capse.jl
@@ -5,13 +5,31 @@ using AbstractCosmologicalEmulators
 
 abstract type AbstractCℓEmulators end
 
+"""
+    w0waCDMCosmology(TrainedEmulator::AbstractTrainedEmulators, ℓgrid::Array,
+    InMinMax::Matrix, OutMinMax::Matrix)
+
+- TrainedEmulator::AbstractTrainedEmulators, the trained emulator
+
+- ℓgrid::Array, the ``\\ell``-grid the emulator has been trained on.
+
+- InMinMax::Matrix, the `Matrix` containing the numbers to perform the MinMax normalization
+of the input features
+
+- OutMinMax::Matrix, the `Matrix` containing the numbers to perform the MinMax normalization
+of the input features
+"""
 @kwdef mutable struct CℓEmulator <: AbstractCℓEmulators
     TrainedEmulator::AbstractTrainedEmulators
     ℓgrid::Array
     InMinMax::Matrix
-    OutMinMax::Array
+    OutMinMax::Matrix
 end
 
+"""
+    get_Cℓ(CℓEmulator::AbstractCℓEmulators)
+Compute the ``C_\ell``on the ``\\ell``-grid the emulator has been trained on.
+"""
 function get_Cℓ(input_params, CℓEmulator::AbstractCℓEmulators)
     input = deepcopy(input_params)
     maximin_input!(input, CℓEmulator.InMinMax)
@@ -20,6 +38,10 @@ function get_Cℓ(input_params, CℓEmulator::AbstractCℓEmulators)
     return output .* exp(input_params[1]-3.)
 end
 
+"""
+    get_ℓgrid(CℓEmulator::AbstractCℓEmulators)
+Return the ``\\ell``-grid the emulator has been trained on.
+"""
 function get_ℓgrid(CℓEmulator::AbstractCℓEmulators)
     return CℓEmulator.ℓgrid
 end

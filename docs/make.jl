5a700d64b5d0c4763a00fe9819c18254d05e3cc3
Marco Bonici <bonici.marco@gmail.com>
2025-09-03 00:06:19 -0400

    Fixing docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,12 +12,7 @@ makedocs(
     modules = [Capse],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        sidebar_sitename = false,
-        canonical = "https://cosmologicalemulators.github.io/Capse.jl/stable",
-        assets = String[],
-        analytics = "UA-XXXXXXXXX-X",  # Add Google Analytics ID if available
-        collapselevel = 2,
-        footer = "Capse.jl v$(pkgversion(Capse)) | [GitHub](https://github.com/CosmologicalEmulators/Capse.jl)"
+        sidebar_sitename = false
     ),
     sitename = "Capse.jl",
     authors = "Marco Bonici, Federico Bianchini, Jaime Ruiz-Zapatero, and contributors",
@@ -36,21 +31,9 @@ makedocs(
         "Python Integration" => "index.md#python-integration",
         "Troubleshooting" => "index.md#troubleshooting",
         "Contributing" => "index.md#contributing"
-    ],
-    repo = "https://github.com/CosmologicalEmulators/Capse.jl/blob/{commit}{path}#{line}",
-    strict = false,  # Set to true in CI to catch documentation errors
-    checkdocs = :exports,
-    linkcheck = :true,
-    linkcheck_ignore = [
-        "https://zenodo.org/record/8187935"  # Ignore Zenodo links that may be slow
     ]
 )
 
-# Deploy documentation
 deploydocs(
     repo = "github.com/CosmologicalEmulators/Capse.jl.git",
-    devbranch = "develop",
-    push_preview = true,
-    forcepush = true,
-    versions = ["stable" => "v^", "v#.#", "dev" => "develop"]
-)
+    devbranch = "develop"
